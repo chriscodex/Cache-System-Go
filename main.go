7b14d989a7780bf6bfd27502996857fb96100ef9
Christian <3003html@gmail.com>
2022-07-11 20:25:13 -0500

    Method Get finished

--- main.go
+++ main.go
@@ -29,7 +29,12 @@ func NewCache(f Function) *Memory {
 }
 
 func (m *Memory) Get(key int) (interface{}, error) {
-
+	result, exists := m.cache[key]
+	if !exists {
+		result.value, result.err = m.f(key)
+		m.cache[key] = result
+	}
+	return result.value, result.err
 }
 
 func main() {
