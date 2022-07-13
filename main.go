24c1512857cb1dcbe04b2b059143870b30329518
Christian <3003html@gmail.com>
2022-07-12 22:04:45 -0500

    Method Get of memory modified

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func NewCache(f Function) *Memory {
 func (m *Memory) Get(key int) (interface{}, error) {
 	result, exists := m.cache[key]
 	if !exists {
-		result.value, result.err = m.f(key)
+		result.value, result.err = m.f(m, key)
 		m.cache[key] = result
 	}
 	return result.value, result.err
